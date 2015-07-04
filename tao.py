46d914feacd4b9397e34e7ef2c8784526f52a2f9
Chester Holtz <chesterholtz@gmail.com>
2015-07-04 18:35:20 -0400

    initial commit

---
+++ tao.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import urllib2
+from random import randint
+
+# Open HTML file
+url = "http://www.mit.edu/~xela/tao.html"
+page = urllib2.urlopen(url)
+doc = BeautifulSoup(page.read())
+
+# Prepare array to store data
+entries = []
+
+# Find all 'h4' tags
+for section in doc.find_all('h4'):
+
+  # Get header text
+  header = section.find_all(text=True)[0]
+
+  # Get paragraph content
+  # ... don't forget about Unicode
+  content = u""
+
+  # Find next tag
+  for p in section.find_next_siblings():
+
+    # ... if it's 'h4' tag - then stop, as we reached next proverb
+    if p.name == 'h4':
+      break
+
+    # Take care of newline characters
+    # ... and tell Python to treat it as Unicode
+    content += unicode(p.getText())
+
+# Add new header plus its content into array
+  entries.append({ 'header': header, 'content': content})
+
+# Chose randomly from set of proverbs
+rand = randint(0,len(entries)-1)
+
+print '\033[1m'
+print(entries[rand].get('header'))
+print '\033[0m'
+print(entries[rand].get('content'))
