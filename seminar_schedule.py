1eec62a5a6fc5aca2161efafc32c1b801babfdbe
Chester Holtz <chesterholtz@gmail.com>
2015-09-21 01:25:15 -0400

    seminar_schedule.py

--- seminar_schedule.py
+++ seminar_schedule.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python
 
+import sys
+import requests
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+
+def unicode_truncate(s, length, encoding='utf-8'):
+    encoded = s.encode(encoding)[:length]
+    return encoded.decode(encoding, 'ignore')
+
+request = requests.get('http://www.cs.rochester.edu/dept/seminar/rss')
+soup = BeautifulSoup(request.text, "lxml")
+items = soup.find_all('item')
+for item in items:
+    title = item.find('title').text
+    link = item.find('guid').text
+    description = item.find('description').text
+    decription = unicode_truncate(description, 75)
+    date = item.find('ev:startdate').text
+    pdate = parse(date)
+    print title + ' - ' + link + ' - ' + pdate.strftime('%d/%m - %H:%M')
+#    print description
